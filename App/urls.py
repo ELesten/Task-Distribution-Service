aa893b7335506e9bc45d4e890a36b223893b90e6
Evgenii Bobarniov <kabanos1@yandex.ru>
2022-08-18 18:27:16 +0300

    fix:Corrected the link. Added a pip file.

--- App/urls.py
+++ App/urls.py
@@ -6,18 +6,6 @@ from drf_yasg.views import get_schema_view
 from .views import *
 from rest_framework import routers, permissions
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 
 router = routers.DefaultRouter()
 router.register(r"comments", TaskCommentView)
@@ -32,17 +20,4 @@ urlpatterns = [
     path("task/", TaskAPIView.as_view()),
     path("all-users/", UsersList.as_view()),
     path("change-user-team/<int:pk>/", ChangeUserTeam.as_view()),
-    re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    re_path(
-        r"^swagger/$",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    re_path(
-        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
-    ),
 ]
