de910a2cf35cd98806b65d07b7c524daa4b67fd6
Дмитрий Чадаев <chadaevd@mail.ru>
2022-08-18 19:19:05 +0300

    fix: resolve conflicts

--- App/admin.py
+++ App/admin.py
@@ -2,6 +2,32 @@ from django.contrib import admin
 from .models import Team, TaskImage, Task, TaskComment, CustomUser
 
 
+class TaskCommentInline(admin.TabularInline):
+    model = TaskComment
+
+
+class TaskAdmin(admin.ModelAdmin):
+    filter_horizontal = ('image', )
+    inlines = [
+        TaskCommentInline,
+    ]
+
+
+class CustomUserInline(admin.TabularInline):
+    model = CustomUser
+    fields = ('username', 'first_name', 'last_name', )
+
+
+class TeamAdmin(admin.ModelAdmin):
+    fields = ('team_name', 'managers')
+    filter_horizontal = ('managers', )
+
+    inlines = [
+        CustomUserInline,
+    ]
+
+
+admin.site.register(Team, TeamAdmin)
 admin.site.register(Team)
 admin.site.register(TaskImage)
 admin.site.register(Task)
